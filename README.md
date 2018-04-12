d2e6fa237f949b365539f40672c1be6c0de5004d
Carlos Castro <alu0100819847@ull.edu.es>
2018-04-12 16:39:15 +0100

    Cancelada

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Práctica 6: RIJNDAEL
+# Práctica 6: RIJNDAEL (CANCELADA)
 
 ### OBJETIVO
 
-  
+INCOMPLETA
 
 
 [Despliegue](https://alu0100819847.github.io/RIJNDAEL/)
--- practicas/js/menu.js
+++ practicas/js/menu.js
@@ -1,8 +1,8 @@
 
 function myMenu(){
-  var menu = ["practicas/1-Vernam/Vernam.html", "practicas/2-Vigenere/Vigenere.html", "practicas/3-RC4/RC4.html", "practicas/4-Mult/Multiplicador.html", "practicas/5-E0-Bluetooth/Generador.html"]
-  var name = ["Practica 1", "Practica 2", "Practica 3", "Practica 4", "Practica 5"]
-  var frontMenu = ["Practica 1: Vernam", "Practica 2: Vigenere", "Practica 3: RC4", "Práctica 4: Multiplicador(AES, SNOW)", "Práctica 5: Generador E0 de Bluetooth"];
+  var menu = ["practicas/1-Vernam/Vernam.html", "practicas/2-Vigenere/Vigenere.html", "practicas/3-RC4/RC4.html", "practicas/4-Mult/Multiplicador.html", "practicas/5-E0-Bluetooth/Generador.html", "practicas/6-RIJNDAEL/RIJNDAEL.html"]
+  var name = ["Practica 1", "Practica 2", "Practica 3", "Practica 4", "Practica 5", "Practica 6"]
+  var frontMenu = ["Practica 1: Vernam", "Practica 2: Vigenere", "Practica 3: RC4", "Práctica 4: Multiplicador(AES, SNOW)", "Práctica 5: Generador E0 de Bluetooth", "Práctica 6: RIJNDAEL (AES)"];
   for(var i = 0; i < menu.length; i++){
     if(menu[i] != ""){
       crearEntradaMenu(menu[i], name[i], i, frontMenu[i]);
